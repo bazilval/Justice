0324288bcb185a36680821aea11cfd283595b62c
bazilval <bazilval@bk.ru>
2017-08-15 16:49:38 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1502804926,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/hud.png",
+		"images/background.png",
+		"images/gameback.png",
+		"images/gamefront.png",
+		"images/eagle-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/player-sheet2.png",
+		"images/cam-sheet0.png",
+		"images/wall.png",
+		"images/mask-sheet0.png",
+		"images/player2-sheet0.png",
+		"images/borrow-sheet0.png",
+		"images/judge-sheet0.png",
+		"images/judge-sheet1.png",
+		"images/box-sheet0.png",
+		"images/рот-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
